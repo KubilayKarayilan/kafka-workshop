dafbaf57dff568637e7edcf74b7f29c2ebe3b773
kubilay karayilan <kubilay.karayilan@ruter.no>
2021-04-24 21:33:01 +0200

    First commit

---
+++ src/main/kotlin/no/inmeta/kafkaworkshop/KafkaWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package no.inmeta.kafkaworkshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaWorkshopApplication
+
+fun main(args: Array<String>) {
+    runApplication<KafkaWorkshopApplication>(*args)
+}
