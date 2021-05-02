fb52257c0386a82c93616218272b168c0249b41b
kubilay karayilan <kubilay.karayilan@ruter.no>
2021-05-02 18:33:48 +0200

    consumer cleanup

--- src/main/kotlin/no/inmeta/kafkaworkshop/conf/KfConsumer.kt
+++ src/main/kotlin/no/inmeta/kafkaworkshop/conf/KfConsumer.kt
@@ -5,11 +5,10 @@ import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 
 @Component
-class KfConsumerFactory {
+class KfConsumer {
     @Value(value = "\${kafka.bootstrapAddress}")
     lateinit var bootstrapAddress: String
 
