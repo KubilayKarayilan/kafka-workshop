3e68edf88394dea794a84e72e2d5d05341e23301
kubilay karayilan <kubilay.karayilan@ruter.no>
2021-05-20 14:50:36 +0200

    streams start

--- src/main/kotlin/no/inmeta/kafkaworkshop/KafkaWorkshopApplication.kt
+++ src/main/kotlin/no/inmeta/kafkaworkshop/KafkaWorkshopApplication.kt
@@ -6,8 +6,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KafkaWorkshopApplication
-@Autowired
-lateinit var kafkaService: KafkaService
+
 fun main(args: Array<String>) {
     runApplication<KafkaWorkshopApplication>(*args)
 }
--- src/main/kotlin/no/inmeta/kafkaworkshop/model/Payment.kt
+++
@@ -1,5 +0,0 @@
-package no.inmeta.kafkaworkshop.model
-
-import java.math.BigDecimal
-
-data class Payment(val orderId:String, val item:String, val amount:BigDecimal)
